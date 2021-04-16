84cfb81f0ba456d75ba2cdec83106d6917bce01c
Andrea Ceroni <andre.kiba@gmail.com>
2021-04-16 07:20:52 +0200

    update

--- k8s/demo3/script.js
+++ k8s/demo3/script.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-    http.post('http://fantasticbike.westeurope.cloudapp.azure.com/buy/api/buy');
+    http.post('http://fantasticbike.westeurope.cloudapp.azure.com/api/buy');
     sleep(1);
 }
